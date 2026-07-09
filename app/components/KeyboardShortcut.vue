e247d081b4b3a0aa521a8e4fd0efe72a2ff13b07
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 02:48:31 +0800

    feat(KeyboardShortcut): add + separator between multi-key shortcuts

--- app/components/KeyboardShortcut.vue
+++ app/components/KeyboardShortcut.vue
@@ -1,11 +1,12 @@
 <template>
   <div role="group" class="flex items-center gap-1" :aria-label="`Keyboard shortcut: ${keys.join(' + ')}`">
-    <kbd
-      v-for="(key, i) in keys"
-      :key="i"
-      class="inline-flex items-center justify-center font-mono text-[10px] px-1.5 py-0.5 border border-black/20 dark:border-white/20 bg-black/5 dark:bg-white/5 leading-none tracking-wider min-w-[1.25rem]">
-      {{ key }}
-    </kbd>
+    <template v-for="(key, index) in keys" :key="index">
+      <kbd
+        class="inline-flex items-center justify-center font-mono text-[10px] px-1.5 py-0.5 border border-black/20 dark:border-white/20 bg-black/5 dark:bg-white/5 leading-none tracking-wider min-w-[1.25rem]">
+        {{ key }}
+      </kbd>
+      <span v-if="index < keys.length - 1" aria-hidden="true" class="opacity-30 mx-0.5 font-sans text-xs">+</span>
+    </template>
     <span v-if="keys.length > 1" class="sr-only">pressed together</span>
   </div>
 </template>
