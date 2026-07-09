668331b27d236cbd2f6e19429c471ed4744af9b2
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 03:30:22 +0800

    feat(components): create AspectRatio component

---
+++ app/components/AspectRatio.vue
@@ -0,0 +1,13 @@
+<template>
+  <div :style="{ aspectRatio: ratio }" class="overflow-hidden">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  ratio?: string
+}>(), {
+  ratio: '16/9'
+})
+</script>
