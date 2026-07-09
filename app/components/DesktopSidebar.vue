0eeb0d876bc065195c9f5aa08a3f023fd8184d62
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 03:23:33 +0800

    style(DesktopSidebar): change availability text from text-[9px] to text-[10px]

--- app/components/DesktopSidebar.vue
+++ app/components/DesktopSidebar.vue
@@ -50,7 +50,7 @@
         <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-60" />
         <span class="relative inline-flex rounded-full w-1.5 h-1.5 bg-green-500" />
       </span>
-      <span class="text-[9px] opacity-50 tracking-[0.15em] uppercase">Open to work</span>
+      <span class="text-[10px] opacity-50 tracking-[0.15em] uppercase">Open to work</span>
     </div>
   </aside>
 </template>
