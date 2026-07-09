71e6aa6c266be3ad2713e8e3d2759b3398410c8a
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 03:13:26 +0800

    style(AppNav): increase inactive opacity from 0.40 to 0.50

--- app/components/AppNav.vue
+++ app/components/AppNav.vue
@@ -12,7 +12,7 @@
         direction === 'vertical'
           ? isActive(link.href)
             ? 'border-l-2 border-current pl-3 py-1.5 opacity-100'
-            : 'pl-2 py-1.5 opacity-40 hover:opacity-70'
+            : 'pl-2 py-1.5 opacity-50 hover:opacity-70'
           : isActive(link.href)
             ? 'font-bold opacity-100'
             : 'opacity-40 hover:opacity-70',
