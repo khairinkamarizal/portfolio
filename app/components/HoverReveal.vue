38b05dba522c214a42bb8d464369a31bd49f429a
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 03:45:52 +0800

    feat(components): add HoverReveal component

---
+++ app/components/HoverReveal.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="group relative overflow-hidden">
+    <div class="transition-transform duration-300 group-hover:-translate-y-full">
+      <slot name="default" />
+    </div>
+    <div class="absolute inset-0 translate-y-full transition-transform duration-300 group-hover:translate-y-0">
+      <slot name="reveal" />
+    </div>
+  </div>
+</template>
