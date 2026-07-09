d55e63e28a71ae694fa324901cadca75aea22bf6
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-09 15:42:09 +0800

    feat(components): add PageHeader reusable page header component

---
+++ app/components/PageHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <h1 class="text-xs opacity-50 tracking-widest">{{ title }}</h1>
+    <p v-if="description" class="text-2xl leading-tight dark:font-light">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description?: string
+}>()
+</script>
