7d119d7d7742dc8eb65a86bb2d82c6e6fef17c17
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 03:30:39 +0800

    feat(components): create Separator component

---
+++ app/components/Separator.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    role="separator"
+    :aria-orientation="orientation"
+    :class="[
+      orientation === 'horizontal'
+        ? 'w-full h-px'
+        : 'h-full w-px',
+      'bg-black/8 dark:bg-white/8',
+      className
+    ]" />
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  orientation?: 'horizontal' | 'vertical'
+  className?: string
+}>(), {
+  orientation: 'horizontal'
+})
+</script>
