860208b09c4ca0042b25f168d96483da72dd6e09
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 03:47:52 +0800

    feat(components): add Marquee component (alias for MarqueeTicker)

---
+++ app/components/Marquee.vue
@@ -0,0 +1,8 @@
+<template>
+  <MarqueeTicker v-bind="$attrs" />
+</template>
+
+<script setup lang="ts">
+// Alias for MarqueeTicker with inheritAttrs passthrough
+defineOptions({ inheritAttrs: false })
+</script>
