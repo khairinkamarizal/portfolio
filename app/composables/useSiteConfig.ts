25f4bd24ea853eddee9cd1fdae9ec61adc1c8c6e
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-09 16:11:06 +0800

    feat(composables): add useSiteConfig composable

---
+++ app/composables/useSiteConfig.ts
@@ -0,0 +1,14 @@
+export const useSiteConfig = () => {
+  return {
+    siteName: 'Khairinkamarizal',
+    siteUrl: 'https://khair.ink',
+    siteDescription: 'Ambitious and versatile creative designer skilled in branding, UI/UX, motion content and web development.',
+    author: 'Khairinkamarizal',
+    email: 'khairinkamarizal@gmail.com',
+    social: {
+      instagram: 'https://instagram.com/khairinkamarizal',
+      behance: 'https://be.net/khairinkamarizal',
+      linkedin: 'https://linkedin.com/in/khairinkamarizal',
+    },
+  }
+}
