53aa900e8ed17325641780888429a00577ce87ac
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-09 16:14:30 +0800

    refactor(ColorToggle): use useTheme composable

--- app/components/ColorToggle.vue
+++ app/components/ColorToggle.vue
@@ -2,19 +2,12 @@
   <ClientOnly>
     <button
       id="color-mode-toggle"
-      @click="toggle"
+      @click="toggleTheme"
       class="flex items-center transition-all duration-300 bg-neutral-50 h-5 w-5 dark:bg-neutral-50 justify-center"
       :aria-label="`Switch to ${isDark ? 'light' : 'dark'} mode`"></button>
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
-import { Circle, CircleDot } from "lucide-vue-next";
-const colorMode = useColorMode();
-
-const isDark = computed(() => colorMode.value === "dark");
-
-const toggle = () => {
-  colorMode.preference = isDark.value ? "light" : "dark";
-};
+const { isDark, toggleTheme } = useTheme()
 </script>
