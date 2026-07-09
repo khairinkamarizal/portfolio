66fd46e003886826208721b5fda9c97b11402e94
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 04:05:47 +0800

    feat(writing/index): add sort by date (newest first) to posts

--- app/pages/writing/index.vue
+++ app/pages/writing/index.vue
@@ -90,7 +90,7 @@
         <!-- Grid -->
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-px bg-black/8 dark:bg-white/8 border border-black/8 dark:border-white/8 divide-y divide-black/8 dark:divide-white/8 max-w-2xl">
           <NuxtLink
-            v-for="(post, index) in filteredPosts"
+            v-for="(post, index) in sortedPosts"
             :key="post.slug"
             :to="`/writing/${post.slug}`"
             class="group bg-white dark:bg-black p-6 flex flex-col gap-4 hover:bg-black/5 dark:hover:bg-white/5 transition-colors duration-150 hover:translate-x-0.5 transition-transform duration-200">
