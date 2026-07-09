3c44e951d8a09093050b5e53f89abf0e2a047c82
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-09 15:47:52 +0800

    feat(components): add SkillBadge component

---
+++ app/components/SkillBadge.vue
@@ -0,0 +1,11 @@
+<template>
+  <span class="text-xs border border-black/20 dark:border-white/20 px-2.5 py-1">
+    {{ label }}
+  </span>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label: string
+}>()
+</script>
