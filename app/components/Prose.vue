70b10fa236275839409feed18dd16b333c497c07
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 03:30:34 +0800

    feat(components): create Prose component wrapper

---
+++ app/components/Prose.vue
@@ -0,0 +1,13 @@
+<template>
+  <div :class="['prose prose-sm max-w-none', dark ? 'dark:prose-invert' : '']">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  dark?: boolean
+}>(), {
+  dark: true
+})
+</script>
