d2736c4e32c40c55bbf25b29bd8bf0abadfeb448
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 02:23:50 +0800

    fix(AppLogo): add aria-hidden and focusable=false to decorative SVG

--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -3,7 +3,8 @@
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 500 500"
     fill="currentColor"
-    aria-label="Logo">
+    aria-hidden="true"
+    focusable="false">
     <polygon
       points="124.6 250 0 250 125.2 375 0 500 125 500 312.6 312.6 500 500 500 375 375.2 250 250 250 187.7 312.7 124.6 250" />
     <polygon
