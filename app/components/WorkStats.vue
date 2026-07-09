76a8a543d9ea6335292a23bceefa2a3439581586
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 03:24:54 +0800

    style(WorkStats): add px-4 to each stat cell

--- app/components/WorkStats.vue
+++ app/components/WorkStats.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-row border border-black/8 dark:border-white/8 divide-x divide-black/8 dark:divide-white/8">
-    <div v-for="stat in stats" :key="stat.label" class="flex-1 flex flex-col gap-0.5 px-6 py-4">
+    <div v-for="stat in stats" :key="stat.label" class="flex-1 flex flex-col gap-0.5 px-4 py-4">
       <span class="text-3xl font-light tracking-tight tabular-nums">{{ stat.displayed }}</span>
       <span class="text-[10px] tracking-[0.15em] opacity-50 font-mono">{{ stat.label.toUpperCase() }}</span>
     </div>
