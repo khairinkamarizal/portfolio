30280badd7637bfe8824203747017d0e7de9c02c
khairinkamarizal <khairinkamarizal@gmail.com>
2026-07-10 17:53:26 +0800

    Add site discovery files

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,37 +1,60 @@
+const SITE_URL = 'https://khair.ink'
+
+interface SitemapEntry {
+  path: string
+  lastmod?: string
+}
+
+function escapeXml(value: string) {
+  return value
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&apos;')
+}
+
+function normaliseDate(value?: string) {
+  if (!value) return undefined
+
+  const date = new Date(value)
+  return Number.isNaN(date.getTime()) ? undefined : date.toISOString().split('T')[0]
+}
+
 export default defineEventHandler(async (event) => {
-  const today = new Date().toISOString().split('T')[0]
-  
-  // Static pages
-  const staticPages = [
-    '/',
-    '/about',
-    '/work',
-    '/writing',
-    '/message',
-    '/brutalist',
+  const staticPages: SitemapEntry[] = [
+    { path: '/' },
+    { path: '/about' },
+    { path: '/work' },
+    { path: '/writing' },
+    { path: '/message' },
+    { path: '/brutalist' },
   ]
-  
-  // Query dynamic writing posts
-  const posts = await queryCollection('writing')
+
+  const posts = await queryCollection(event, 'writing')
     .where('draft', '=', false)
     .all()
-  
-  const postUrls = posts.map((post) => {
-    const slug = post.stem?.split('/').pop() || ''
-    return `/writing/${slug}`
-  })
-  
-  const allUrls = [...staticPages, ...postUrls]
-  
-  // Build XML
+
+  const postPages: SitemapEntry[] = posts.map((post) => ({
+    path: post.path || `/writing/${post.stem?.split('/').pop() || ''}`,
+    lastmod: normaliseDate(post.date),
+  }))
+
+  const entries = [...staticPages, ...postPages]
+    .filter(({ path }) => path && !path.endsWith('/writing/'))
+    .sort((a, b) => a.path.localeCompare(b.path))
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${allUrls.map((url) => `  <url>
-    <loc>https://khair.ink${url}</loc>
-    <lastmod>${today}</lastmod>
-  </url>`).join('\n')}
+${entries.map(({ path, lastmod }) => {
+    const location = escapeXml(new URL(path, SITE_URL).href)
+    const modified = lastmod ? `\n    <lastmod>${lastmod}</lastmod>` : ''
+    return `  <url>\n    <loc>${location}</loc>${modified}\n  </url>`
+  }).join('\n')}
 </urlset>`
-  
-  event.node.res.setHeader('Content-Type', 'application/xml')
+
+  event.node.res.setHeader('Content-Type', 'application/xml; charset=utf-8')
+  event.node.res.setHeader('Cache-Control', 'public, max-age=3600, s-maxage=86400')
+
   return sitemap
 })
